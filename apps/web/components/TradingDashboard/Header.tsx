ebdd7013f68e988ee33b6964905bdfbe57473c8b
hrithik <hrithikmails88@gmail.com>
2026-05-21 13:44:43 +0530

    feat:added logout button

--- apps/web/components/TradingDashboard/Header.tsx
+++ apps/web/components/TradingDashboard/Header.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import api from "@/lib/axios";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+
 type Props = {
   balance: number;
 };
@@ -7,6 +11,18 @@ type Props = {
 export default function Header({
   balance,
 }: Props) {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await api.post("api/auth/logout");
+
+      router.push("/login");
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
   return (
     <header className="h-16 border-b px-6 flex items-center justify-between">
       <div>
@@ -24,6 +40,12 @@ export default function Header({
           <p className="font-semibold text-lg">
             ${balance}
           </p>
+        </div>
+
+        <div>
+          <Button 
+            className="cursor-pointer"
+            onClick={handleLogout}>Logout</Button>
         </div>
       </div>
     </header>
